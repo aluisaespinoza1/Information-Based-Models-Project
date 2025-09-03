36ea8942f90ffe1bd62301c1c81aca8647e63a40
Ana Luisa Espinoza López <ana.espinoza@iteso.mx>
2025-09-03 14:34:01 -0600

    liquidity prob functions

--- utils.py
+++ utils.py
@@ -8,3 +8,15 @@ from scipy.optimize import minimize
 # Función distribución Erlang
 def erlang_pdf(x, lambda_, k):
     return np.where(x < 0, 0, (lambda_**k * x**(k-1) * np.exp(-lambda_*x)) / math.factorial(k-1))
+
+#Liquidity functions buy and sell
+
+#liquidity buy
+def p_LB(S):
+    return max(0, min(0.5, 0.5 - 0.08 * (S))) #S= (A-S0)
+
+#liquidity sell
+def p_LS(S):
+    return max(0, min(0.5, 0.5 - 0.08 * (S))) #S= (S0-B)
+
+
